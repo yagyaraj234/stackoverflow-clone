71880ce8ec1433f20264749b8d18299a6a94ff40
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-08-25 16:01:33 +0530

    footer added

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import Footer from "./Footer";
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
+      <div className="min-h-[30vh]">{children}</div>
+      <Footer />
     </>
   );
 };
