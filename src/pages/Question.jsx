2c63d246c6c443562f9f2d037cf043267d9670aa
Yagyaraj Lodhi <101352591+yagyaraj234@users.noreply.github.com>
2023-08-27 21:51:59 +0530

    Api integration (#3)
    * "Api get requestion to stackexchange"
    
    * search api integration
    
    * question a pi integration from using stack exchange api
    
    * added debounce package to avoid debouncing while searching content loader

--- src/pages/Question.jsx
+++ src/pages/Question.jsx
@@ -1,9 +1,22 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 import { BsHandThumbsUp, BsChatLeft, BsEye } from "../icons/index";
-// Local data from json file
-import data from "./quest.json";
 
 const Question = () => {
+  const [data, setData] = useState();
+  const apiUrl =
+    "https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow";
+  const fetchData = () => {
+    axios.get(apiUrl).then((res) => {
+      // console.log(res.data.items);
+      setData(res.data.items);
+    });
+  };
+
+  useEffect(() => {
+    fetchData();
+  });
   return (
     <div className="text-justify w-full z-0 ">
       {/* Head title */}
@@ -31,8 +44,7 @@ const Question = () => {
           month
         </li>
       </div>
-      {/*  */}
-      {data?.items.map((item) => (
+      {data?.map((item) => (
         <div
           className="flex justify-between py-5 border-b px-10"
           key={item.question_id}
